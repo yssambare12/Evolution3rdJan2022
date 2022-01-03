5ec315399a3feed0da5e3da444a19935b98c53b3
Yogesh  Sambare <bloggingscan@gmail.com>
2022-01-03 16:54:20 +0530

    updated

--- recipetrackapp/src/App.js
+++ recipetrackapp/src/App.js
@@ -1,23 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { Form } from './components/Form';
+import { Showing } from './components/showrecipe';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      hello
+      <div className='griding'>
+      <div className='left_form'> 
+      <h3>Enter reciepe details</h3>
+      <Form className="lef"/></div>
+      <div><Showing/></div>
+      </div>
+      
+     
     </div>
   );
 }
---
+++ recipetrackapp/src/components/Form.jsx
@@ -0,0 +1,54 @@
+import { useState , useRef} from "react"
+import { nanoid } from "nanoid";
+import { Showing } from "./showrecipe";
+export const Form=()=>{
+    const [form,setForm]=useState({
+        title:"", 
+        ingredients:"", 
+        time_to_cook:"", 
+        image:"",
+        instructions:""
+    })
+    const [list,setList]=useState([]);
+    const ref=useRef("");
+    const handleChange=(e)=>{
+     const {name,value}=e.target;
+    setForm({...form,[name]:value});
+    }
+    const handleFileChange=(e)=>{
+        console.log(ref.current.files[0].name)
+        setForm({...form,image:ref.current.files[0].name});
+    }
+    const handleSubmit=(e)=>{
+        e.preventDefault();
+        console.log(ref.current.files[0].name)
+        console.log(form);
+        const payload={
+        title:form.title, 
+        ingredients:form.ingredients, 
+        time_to_cook:form.time_to_cook, 
+        image:form.image,
+        instructions:form.instructions,
+        id:nanoid(7)
+        }
+        setList([...list,payload]);
+        fetch('http://localhost:3000/posts', {
+            method: 'POST',
+            body: JSON.stringify(payload),
+            headers: {
+                'content-type': 'application/json'
+            }
+        }).then(() => console.log("done"));
+       
+    }
+    return (
+        <form onSubmit={handleSubmit} >
+            <input onChange={handleChange} name="title" type="text" placeholder="title" style={{width:"100%"}}/>
+            <input onChange={handleChange} name="ingredients" type="text" placeholder="ingredients" style={{width:"100%"}}/>
+            <input onChange={handleChange} name="time_to_cook" type="text" placeholder="time_to_cook" style={{width:"100%"}}/>
+            <input onChange={handleChange} name="instructions" type="text" placeholder="instructions" style={{width:"100%"}}/>     
+            <input ref={ref} onChange={handleFileChange} type="file" style={{width:"100%"}}/> 
+            <input type="submit" value="submit"/>
+        </form>
+    )
+}
